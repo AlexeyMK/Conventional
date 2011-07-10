0886647b5c5d668cd9c0d9c6d2fb586833b1543f
Alexandru Damian <alexdamian1989@gmail.com>
2011-07-10 06:09:07 +0300

    Add the yaml parser

--- conventional.py
+++ conventional.py
@@ -1,6 +1,11 @@
 from jinja2 import Template
 from os import mkdir
-yaml_input = {"project_name":"example", "indentation_style": "spaces", "num_spaces":"4"}
+import sys
+import yaml
+
+yaml_input = yaml.load(open(sys.argv[1]).read())
+
+print yaml_input
 
 f = open("python_templates/python.vimrc.template")
 
@@ -9,4 +14,3 @@ output = open(yaml_input['project_name'] + '/python.vimrc', 'w')
 output.write(Template("".join(f.readlines())).render(yaml_input))
 output.close()
 print "Woot, it worked!"
-
