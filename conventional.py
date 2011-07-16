b9e98c188482fc26cc3d4df21cdd18ab2e2c27ad
Alexandru Damian <alexdamian1989@gmail.com>
2011-07-16 15:29:14 +0300

    Ditch YAML and use JSON for everything.

--- conventional.py
+++ conventional.py
@@ -5,25 +5,24 @@ import yaml
 import json
 
 
-json_config_file = "config.json"
-json_raw_data = open(json_config_file);
-json_data = json.load(json_raw_data);
-json_raw_data.close()
+global_config_file = "config.json"
+user_config_file = "user_config.json"
 
-yaml_input = yaml.load(open(sys.argv[1]).read())
+json_data = json.load(open(global_config_data)
+languages = json_data["supported_languages"]
 
-languages = json_data["supported_languages"];
+user_input = json.load(open(sys.argv[1]))
 
-mkdir(yaml_input['ProjectName'])
+mkdir(user_input['ProjectName'])
 
 for language in languages:
-	if not (language in yaml_input):
+	if not (language in user_input):
 		continue
   print "Creating files for " + language
   # generating vimrc
   f = open("templates/" + language + ".vimrc.template")
-  output = open(yaml_input['ProjectName'] + '/' + language +'.vimrc', 'w')
-  output.write(Template("".join(f.readlines())).render(yaml_input[language]))
+  output = open(user_input['ProjectName'] + '/' + language +'.vimrc', 'w')
+  output.write(Template("".join(f.readlines())).render(user_input[language]))
   output.close()
 
 print "Woot, it worked!"
